fe2b78244c94fb757b6bcd76ada5449ed745d158
Ronel Andaya <ronel.andaya@gmail.com>
2022-09-24 01:06:58 +0800

    49. Working with State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import ExpenseItem from "./components/ExpenseItem";
+
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
     { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 12) },
-    { title: "Toilet Paper", amount: 294.67, date: new Date(2021, 2, 12) },
-    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 12) },
-    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 12) },
+    { title: "Toilet Paper", amount: 300.00, date: new Date(2021, 3, 12) },
+    { title: "Candies", amount: 400.67, date: new Date(2021, 4, 12) },
+    { title: "Chocolates", amount: 250.68, date: new Date(2021, 5, 12) },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
+      <Expenses item={expenses}></Expenses>
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,22 @@
+import React, {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
+import  Card from '../UI/Card';
 import './ExpenseItem.css';
-
 function ExpenseItem(props) {
-    
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = ()=>{
+    setTitle('Updated');
+  }
+  
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
+        <button onClick={clickHandler}>Test</button>
       </div>
-    </div>
+    </Card>
   );
 }
 
