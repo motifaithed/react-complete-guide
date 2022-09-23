79bc16167afd3fc05f031106225a0150e020849a
Ronel Andaya <ronel.andaya@gmail.com>
2022-09-24 02:00:07 +0800

    53. Listening to User Input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 
+import React from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
@@ -10,7 +12,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <Expenses item={expenses}></Expenses>
     </div>
   );
