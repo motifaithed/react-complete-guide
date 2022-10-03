ffb5cdccf41db036ec9a7c950fde6dcb67612c45
Ronel Andaya <ronel.andaya@gmail.com>
2022-10-03 14:07:51 +0800

    64. Rendering Lists of Data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,46 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
-import Card from '../UI/Card';
-import './Expenses.css';
+import Card from "../UI/Card";
+import "./Expenses.css";
 
 function Expenses(props) {
-  
-  const [filteredYear, setFilteredYear ] = useState("2019");
+  const [filteredYear, setFilteredYear] = useState("2019");
   console.log(filteredYear);
-  const setFilteredYearHandler = (receivedYear)=>{
+  const setFilteredYearHandler = (receivedYear) => {
     setFilteredYear(receivedYear);
-    console.log('You are at expenses component');
-    
-  }
+    console.log("You are at expenses component");
+  };
   return (
     <div>
-    <Card className="expenses">
-    <ExpensesFilter onSetFilteredYear = {setFilteredYearHandler} selected={filteredYear}></ExpensesFilter>
-    </Card>
-    <Card className="expenses">
-      
-      <ExpenseItem
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-      ></ExpenseItem>
-    </Card>
+      <Card className="expenses">
+        <ExpensesFilter
+          onSetFilteredYear={setFilteredYearHandler}
+          selected={filteredYear}
+        ></ExpensesFilter>
+      </Card>
+      <Card className="expenses">
+        {props.item.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
     </div>
   );
 }
